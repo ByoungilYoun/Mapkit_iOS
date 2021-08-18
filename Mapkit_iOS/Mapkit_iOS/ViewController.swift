6ea7945c320cb89d5a16b655043c2acb112c8dea
YounByoungil <jimmy7525@naver.com>
2021-08-18 12:50:14 +0900

    Zooming into User's Location

--- Mapkit_iOS/Mapkit_iOS/ViewController.swift
+++ Mapkit_iOS/Mapkit_iOS/ViewController.swift
@@ -31,11 +31,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     locationManager.distanceFilter = kCLDistanceFilterNone
     locationManager.startUpdatingLocation()
     locationManager.requestWhenInUseAuthorization()
+    
+    self.mapView.delegate = self
+    
     self.mapView.showsUserLocation = true
     
     view.addSubview(mapView)
     mapView.snp.makeConstraints {
       $0.top.leading.trailing.bottom.equalToSuperview()
     }
+  }
+}
+
+  //MARK: - extension MKMapViewDelegate
+extension ViewController : MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+    let region = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.008, longitudeDelta: 0.008))
+    mapView.setRegion(region, animated: true)
   }
 }
