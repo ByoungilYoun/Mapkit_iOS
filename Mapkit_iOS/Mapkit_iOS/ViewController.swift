66f191270575afbdf794f2d6130cb7a8791986d7
YounByoungil <jimmy7525@naver.com>
2021-08-18 11:10:33 +0900

    Setup Project

---
+++ Mapkit_iOS/Mapkit_iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mapkit_iOS
+//
+//  Created by 윤병일 on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+
+  //MARK: - Functions
+  private func configureUI() {
+    view.backgroundColor = .blue
+  }
+
+}
+
