94430ddb5b98de3771efb457ba35d7b68e43f906
YounByoungil <jimmy7525@naver.com>
2021-08-18 15:47:19 +0900

    Switching Map Types

--- Mapkit_iOS/Mapkit_iOS/ViewController.swift
+++ Mapkit_iOS/Mapkit_iOS/ViewController.swift
@@ -18,10 +18,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
   
   private let locationManager = CLLocationManager()
   
+  let mapTypeSegmentControl : UISegmentedControl = {
+    let sg = UISegmentedControl()
+    sg.insertSegment(withTitle: "Map", at: 0, animated: true)
+    sg.insertSegment(withTitle: "Satelite", at: 1, animated: true)
+    sg.insertSegment(withTitle: "Hybrid", at: 2, animated: true)
+    sg.addTarget(self, action: #selector(segmentControlDidTap), for: .valueChanged)
+    sg.backgroundColor = .white
+    sg.selectedSegmentTintColor = .blue
+    sg.setTitleTextAttributes([.foregroundColor : UIColor.blue], for: .normal)
+    sg.setTitleTextAttributes([.foregroundColor : UIColor.white], for: .selected)
+    return sg
+  }()
+  
   //MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
+    configureSegmentControl()
   }
 
   //MARK: - Functions
@@ -39,6 +53,24 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     view.addSubview(mapView)
     mapView.snp.makeConstraints {
       $0.top.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+  
+  private func configureSegmentControl() {
+    view.addSubview(mapTypeSegmentControl)
+    mapTypeSegmentControl.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+      $0.centerX.equalToSuperview()
+    }
+  }
+  
+  //MARK: - @objc func
+  @objc func segmentControlDidTap(sender : UISegmentedControl) {
+    switch sender.selectedSegmentIndex {
+    case 0 : self.mapView.mapType = .standard
+    case 1 : self.mapView.mapType = .satellite
+    case 2 : self.mapView.mapType = .hybrid
+    default : self.mapView.mapType = .standard
     }
   }
 }
